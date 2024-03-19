e74f4325223ca08d33f6d3c84697c5b6ec4d1158
FaizNala <faiznala2@gmail.com>
2024-03-19 14:02:25 +0700

    Pertemuan 5 - Praktikum 3 (Menggabungkan HTML dan PHP)

---
+++ Pertemuan5_PHP2/PHPHTML1.php
@@ -0,0 +1,8 @@
+<?php
+    echo '<HTML>';
+    echo '<head><title>Cara02</title></head>';
+    echo '<body>';
+    echo '<p> Tanggal Gari ini : ' . date('d M Y') . '</p>';
+    echo '</body>';
+    echo '</HTML>';
+?>
