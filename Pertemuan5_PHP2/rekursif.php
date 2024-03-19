0da11a0b6e0bf25bae081c27a3f576ac9739602e
FaizNala <faiznala2@gmail.com>
2024-03-19 13:27:01 +0700

    Pertemuan 5 - Praktikum 3 (rekursif)

---
+++ Pertemuan5_PHP2/rekursif.php
@@ -0,0 +1,71 @@
+<?php
+    // fungsi rekursif
+    // function tampilkanHaloDunia() {
+    //     echo "Halo dunia <br>";
+
+    //     tampilkanHaloDunia();
+    // }
+
+    // tampilkanHaloDunia();
+
+    // for ($i = 1; $i <= 25; $i++) {
+    //     echo "Perulangan ke-{$i}<br>";
+    // }
+
+    // function tampilkanAngka(int $jumlah, int $indeks = 1) {
+    //     echo "Perulangan ke-{$indeks}<br>";
+
+    //     if ($indeks < $jumlah) {
+    //         tampilkanAngka($jumlah, $indeks + 1);
+    //     }
+    // }
+    // tampilkanAngka(20);
+
+    $menu = [
+        [
+            "nama" => "Beranda"
+        ],
+        [
+            "nama" => "Berita",
+            "subMenu" => [
+                [
+                    "nama" => "Wisata",
+                    "subMenu" => [
+                        [
+                            "nama" => "Pantai"
+                        ],
+                        [
+                            "nama" => "Gunung"
+                        ]
+                    ]
+                ],
+                [
+                    "nama" => "Kuliner"
+                ],
+                [
+                    "nama" => "Hiburan"
+                ]
+            ]
+        ],
+        [
+            "nama" => "Tentang"
+        ],
+        [
+            "nama" => "Kontak"
+        ]
+    ];
+
+    function tampilkanMenubertingkat(array $menu) {
+        echo "<ul>";
+        foreach ($menu as $key => $item) {
+            echo "<li>{$item['nama']}";
+            if (isset($item['subMenu'])) {
+                tampilkanMenubertingkat($item['subMenu']);
+            }
+            echo "</li>";
+        }
+        echo "</ul>";
+    }
+
+    tampilkanMenubertingkat($menu);
+?>
