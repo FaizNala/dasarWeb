2c9bed426ecbd6667813f43724a5656cdf2e5072
FaizNala <faiznala2@gmail.com>
2024-04-07 22:54:36 +0700

    Pertemuan 9 - Praktikum 1

---
+++ Pertemuan9/insert.php
@@ -0,0 +1,31 @@
+<?php
+// Koneksi ke database
+$koneksi = mysqli_connect("localhost", "root", "", "prakwebdb");
+
+// Periksa koneksi
+if (mysqli_connect_errno()) {
+    echo "Koneksi database gagal: " . mysqli_connect_error();
+    exit();
+}
+
+// Data yang akan dimasukkan
+$id = 1;
+$username = "admin";
+$password = "123";
+
+// Hash password menggunakan MD5
+$password_hashed = md5($password);
+
+// Query untuk memasukkan data ke dalam tabel user
+$query = "INSERT INTO user (id, username, password) VALUES ('$id','$username', '$password_hashed')";
+
+// Eksekusi query
+if (mysqli_query($koneksi, $query)) {
+    echo "Data berhasil dimasukkan ke tabel user.";
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
+}
+
+// Tutup koneksi
+mysqli_close($koneksi);
+?>;
